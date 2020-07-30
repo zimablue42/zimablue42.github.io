7df78a7482c37b97805c658db75cc6c25615a707
zimablue42 <437225121@qq.com>
2020-07-30 08:36:01 +0800

    Site updated: 2020-07-30 08:36:00

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
-        ("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+        ("The", "answer", "to", "life", "the universe", "and", "everything", "is", "42");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
