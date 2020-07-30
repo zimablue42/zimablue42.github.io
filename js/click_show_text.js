8bcb6c8417a263a758bc6fef419ec43a9c9c0aca
zimablue42 <437225121@qq.com>
2020-07-30 08:32:50 +0800

    Site updated: 2020-07-30 08:32:50

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
-        ("The", "answer", "to", "life","the universe", "and", "everything", "is", "42");
+        ("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
@@ -13,7 +13,7 @@ jQuery(document).ready(function($) {
             "left": x,
             "position": "absolute",
             "font-weight": "bold",
-            "color": "rgb("125","125","125")"
+            "color": "#FF0000"
         });
         $("body").append($i);
         $i.animate({
